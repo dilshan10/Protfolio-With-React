e81c488878d04c5ea1a47383a990274e9db8af46
dilshan10 <superdilshan999@gmail.com>
2023-06-10 17:03:36 +0530

    Add nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import "./NavBarStyle.css";
+
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+
+import {FaBars,FaTimes} from "react-icons/fa"
+
+const NavBar = () =>{
+
+    const [click,setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    return(
+        <div className="header">
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className={click ? "nav-list active":"nav-list"}>
+                <li>
+                    <Link to ="/">Home</Link>
+                </li>
+                <li>
+                    <Link to ="/about">About</Link>
+                </li>
+                <li>
+                    <Link to ="/skils">Skils</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+
+            <div className="hamburgerMenu" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={20} style={{color:"white"}}/>
+                ):(
+                    <FaBars size={20} style={{color:"white"}}/>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
